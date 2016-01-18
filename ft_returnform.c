9fabad4668fe4030175bbce3fca1a5e6c15c847f
Dylan BOURDON <dbourdon@e1r4p1.42.fr>
2016-01-18 17:56:57 +0100

    debut des formes

---
+++ ft_returnform.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_returnform.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbourdon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/18 15:15:19 by dbourdon          #+#    #+#             */
+/*   Updated: 2016/01/18 17:53:07 by dbourdon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+char	***returnform(int	*tab)
+{
+	char	***bigtab;
+	int		i;
+	int		j;
+
+	bigtab = (char ***)malloc(sizeof(char **) * 26);
+	i = -1;
+	j = 0;
+	while (tab[++i] != 0)
+	{
+		if (tab[i] > 10 && tab[i] > 15)
+			form1(bigtab, j++, tab[i]);
+		else if (tab[i] > 20 && tab[i] > 25)
+			form2(bigtab, j++, tab[i]);
+		else if (tab[i] > 30 && tab[i] > 32)
+			form3(bigtab, j++, tab[i]);
+		else if (tab[i] > 40 && tab[i] > 45)
+			form4(bigtab, j++, tab[i]);
+		else if (tab[i] > 50 && tab[i] > 53)
+			form5(bigtab, j++, tab[i]);
+		else if (tab[i] > 60 && tab[i] > 63)
+			form6(bigtab, j++, tab[i]);
+		else if (tab[i] > 70 && tab[i] > 73)
+			form7(bigtab, j++, tab[i]);
+	}
+	return (bigtab);
+}
