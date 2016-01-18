f77f06f77c9461cfed343a687829377bf318c69d
Dylan BOURDON <dbourdon@e1r4p1.42.fr>
2016-01-18 20:22:17 +0100

    ok

--- form1.c
+++ form1.c
@@ -6,11 +6,25 @@
 /*   By: dbourdon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/18 17:49:15 by dbourdon          #+#    #+#             */
-/*   Updated: 2016/01/18 17:53:38 by dbourdon         ###   ########.fr       */
+/*   Updated: 2016/01/18 20:20:56 by dbourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fillit.h"
+
+void	form11(char ***bigtab, int j)
+{
+	**bigtab[j] = malloc
+}
+
 void	form1(char ***bigtab, int j, int nb)
 {
-	lplplplplplplplplpl
+	if (nb == 11)
+		form11(bigtab, j);
+	if (nb == 12)
+		form12(bigtab, j);
+	if (nb == 13)
+		form13(bigtab, j);
+	if (nb == 14)
+		form14(bigtab, j);
 }
